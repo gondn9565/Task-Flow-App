723dccc1db6c9ac33e1ee30b6d3ac39f3a2acd33
Neha Gond <gondn9565@gmail.com>
2025-09-16 19:45:44 +0530

    Task adding feature added

--- frontend/src/components/TaskModel.jsx
+++ frontend/src/components/TaskModel.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { DEFAULT_TASK } from "../assets/Dummy";
+import {
+  baseControlClasses,
+  DEFAULT_TASK,
+  priorityStyles,
+} from "../assets/Dummy";
 import { useEffect } from "react";
 import { useState } from "react";
+import { AlignLeft, PlusCircle, Save, X } from "lucide-react";
+import axios from "axios";
+import { useCallback } from "react";
+import { Calendar, Flag } from "lucide-react";
+
 const API_BASE = "http://localhost:4000";
 const TaskModel = ({ isOpen, onClose, taskToEdit, onSave, onLogout }) => {
   const [taskData, setTaskData] = useState(DEFAULT_TASK);
@@ -28,6 +37,55 @@ const TaskModel = ({ isOpen, onClose, taskToEdit, onSave, onLogout }) => {
       setTaskData(DEFAULT_TASK);
     }
   }, [isOpen, taskToEdit]);
+  const handleChange = useCallback((e) => {
+    const { name, value } = e.target;
+    setTaskData((prev) => ({ ...prev, [name]: value }));
+  }, []);
+  const getHeaders = useCallback(() => {
+    const token = localStorage.getItem("token");
+    if (!token) throw new Error("No auth Token Found");
+    return {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    };
+  }, []);
+  const handleSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      if (taskData.dueDate < today) {
+        setError("Due date cannot be in the past");
+        return;
+      }
+      setLoading(true);
+      setError(null);
+      try {
+        const isEdit = Boolean(taskData.id);
+        const url = isEdit ? `${API_BASE}/${taskData.id}/gp` : `${API_BASE}/gp`;
+        const resp = await fetch(url, {
+          method: isEdit ? "PUT" : "POST",
+          headers: getHeaders(),
+          body: JSON.stringify(taskData),
+        });
+        if (!resp.ok) {
+          if (resp.status === 401) {
+            return;
+            onLogout();
+            const err = await resp.json();
+            throw new Error(err.message || "Failed to save task");
+          }
+          const saved = await resp.json();
+          onSave(saved);
+          onClose();
+        }
+      } catch (error) {
+        console.log(error);
+        setError(error.message || "An unexpected error occurred");
+      } finally {
+        setLoading(false);
+      }
+    },
+    [taskData, today, getHeaders, onLogout, onSave, onClose]
+  );
   return (
     <div className="fixed inset-0 backdrop-blur-sm bg-black/20 z-50  flex items-center justify-center">
       <div className="bg-white border border-purple-100 rounded-xl max-w-md w-full shadow-lg relative p-6 animate-fadeIn">
@@ -48,7 +106,74 @@ const TaskModel = ({ isOpen, onClose, taskToEdit, onSave, onLogout }) => {
           </button>
         </div>
         {/*FORM TO FILL TO CREATE A TASK */}
-        <form></form>
+        <form onSubmit={handleSubmit} className="space-4">
+          {error && (
+            <div className="text-sm text-red-600 bg-red-50 p-3 rounded-lg border border-red-100">
+              {error}
+            </div>
+          )}
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Task Title
+            </label>
+            <div className="flex items-center border border-purple-100 rounded-lg px-3 py-2.5 focus-within:ring-2 focus-within:ring-purple-500 focus-within:border-purple-500 transition-all duration-200">
+              <input
+                type="text"
+                name="title"
+                required
+                value={taskData.title}
+                onChange={handleChange}
+                className="w-full focus:outline-none text-sm"
+                placeholder="Enter task title"
+              />
+            </div>
+          </div>
+          <div>
+            <label className="flex items-center gap-1 text-sm font-medium text-gray-700 mb-1">
+              {" "}
+              <AlignLeft className="w-4 h-4 text-purple-500" />
+            </label>
+            <texttarea
+              name="description"
+              rows="3"
+              onChange={handleChange}
+              value={taskData.description}
+              className={baseControlClasses}
+              placeholder="Add details about your task"
+            />
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <label className="flex items-cennter gap-1 text-sm font-medium text-gray-700 mb-1">
+                <Flag className="w-4 h-4 text-purple-500" /> Priority
+              </label>
+              <select
+                name="priority"
+                value={taskData.priority}
+                onChange={handleChange}
+                className={`${priorityStyles[taskData.priority]}`}
+              >
+                <option>Low</option>
+                <option>Medium</option>
+                <option>High</option>
+              </select>
+            </div>
+            <div>
+              <label className="flex items-cennter gap-1 text-sm font-medium text-gray-700 mb-1">
+                <Calendar className="w-4 h-4 text-purple-500" /> Due date
+              </label>
+              <input
+                type="date"
+                name="dueDate"
+                required
+                min={today}
+                value={taskData.dueDate}
+                onChange={handleChange}
+                className={baseControlClasses}
+              />
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
